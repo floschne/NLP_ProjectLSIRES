2676d2312c4fb730497b9447ac32b5293a4d7779
Patrick Körber <5koerber@informatik.uni-hamburg.de>
2018-01-19 18:22:27 +0100

    elasticsearch: dev

---
+++ dev/elasticsearch/src/main/java/client/ElasticsearchClient.java
@@ -0,0 +1,78 @@
+package client;
+
+import data.input.WikiArticle;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ElasticsearchClient implements ElasticsearchClientInterface{
+
+    // default config
+    private static final String CLUSTER_NAME =  "docker-cluster";
+    private static final int TRANSPORT_TCP_PORT =  9201;
+    private static final List<String> CLUSTER_SERVERS =  Collections.unmodifiableList(Collections.singletonList("127.0.0.1"));
+
+    private TransportClient client = null;
+
+    /**
+     * Generates Elasticsearch client with default connection
+     *
+     * @throws UnknownHostException UnknownHostException
+     */
+    public ElasticsearchClient() throws UnknownHostException {
+        this.init(CLUSTER_SERVERS, TRANSPORT_TCP_PORT, CLUSTER_NAME);
+    }
+
+    /**
+     * @param serverAddresses List of elasticsearch server addresses (e.g. '127.0.0.1')
+     * @param transportTCPPort The port for the elasticsearch transport protocol. Attention! Not the HTTP port!
+     * @param clusterName Name of the elasticsearch cluster
+     *
+     * @throws UnknownHostException UnknownHostException
+     */
+    public ElasticsearchClient(List<String> serverAddresses, int transportTCPPort, String clusterName) throws UnknownHostException {
+        this.init(serverAddresses, transportTCPPort, clusterName);
+    }
+
+    private void init(List<String> serverAddresses, int transportTCPPort, String clusterName) throws UnknownHostException {
+        Settings settings = Settings.builder()
+                .put("client.transport.sniff", false)
+                .put(ClusterName.CLUSTER_NAME_SETTING.getKey(), clusterName)
+                .build()
+                ;
+
+        this.client = new PreBuiltTransportClient(settings);
+
+        for (String serverAddress : serverAddresses) {
+            this.client.addTransportAddress(new TransportAddress(InetAddress.getByName(serverAddress), transportTCPPort));
+        }
+    }
+
+    public void closeConnection() {
+        this.client.close();
+    }
+
+    @Override
+    public List<String> findArticleTitlesByLanguageCodeAndQuery(String languageCode, String query) {
+        return null; // todo
+    }
+
+    @Override
+    public boolean indexArticle(WikiArticle wikiArticle) {
+        return false; // todo
+    }
+
+    @Override
+    public boolean indexArticles(List<WikiArticle> wikiArticles) {
+        return false; // todo
+    }
+}
