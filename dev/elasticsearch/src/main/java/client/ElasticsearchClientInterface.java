f2cb3959795fa073658d96dd5be9e84b844f7327
Patrick Körber <5koerber@informatik.uni-hamburg.de>
2018-01-19 18:18:36 +0100

    updated interface

--- dev/elasticsearch/src/main/java/client/ElasticsearchClientInterface.java
+++ dev/elasticsearch/src/main/java/client/ElasticsearchClientInterface.java
@@ -5,10 +5,24 @@ import java.util.List;
 
 public interface ElasticsearchClientInterface {
 
-    String[] findArticleTitlesByLanguageCodeAndQuery(String languageCode, String query);
+    /**
+     * @param languageCode Language code that determines the used elasticsearch index. E.g. 'de'
+     * @param query Query string. E.g. 'How old is Donald Trump'
+     *
+     * @return List<String> Returns a list of the IDs from the search results.
+     */
+    List<String> findArticleTitlesByLanguageCodeAndQuery(String languageCode, String query);
 
+    /**
+     * @param wikiArticle Article to index
+     * @return void
+     */
     boolean indexArticle(WikiArticle wikiArticle);
 
+    /**
+     * @param wikiArticles List of the articles to index
+     * @return void
+     */
     boolean indexArticles(List<WikiArticle> wikiArticles);
 
 }
